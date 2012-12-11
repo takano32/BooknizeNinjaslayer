77eeb1eb15d979ee2aa9a622d35ab61cf2c81561
TAKANO Mitsuhiro <takano32@gmail.com>
2012-12-11 16:55:04 +0900

    add NeoSaitama ReST files

--- ninjaslayer.rb
+++ ninjaslayer.rb
@@ -6,47 +6,82 @@ require 'nokogiri'
 require 'open-uri'
 
 class NinjaslayerTogetter
-	@@urls = %w(
+	NeoSaitamaUrls = %w(
 		http://togetter.com/li/73081
+		http://togetter.com/li/67523
+		http://togetter.com/li/73449
+		http://togetter.com/li/73454
+		http://togetter.com/li/238464
+		http://togetter.com/li/75633
+		http://togetter.com/li/73778
+		http://togetter.com/li/74478
+		http://togetter.com/li/75282
+		http://togetter.com/li/78144
+		http://togetter.com/li/75597
+		http://togetter.com/li/75606
+		http://togetter.com/li/75654
+		http://togetter.com/li/75756
+		http://togetter.com/li/77433
+		http://togetter.com/li/77689
+		http://togetter.com/li/77702
+		http://togetter.com/li/72801
+		http://togetter.com/li/77727
+		http://togetter.com/li/87853
+		http://togetter.com/li/94537
+		http://togetter.com/li/98062
+		http://togetter.com/li/98696
+		http://togetter.com/li/109984
+		http://togetter.com/li/111442
+		http://togetter.com/li/121054
+		http://togetter.com/li/121698
+		http://togetter.com/li/81948
+		http://togetter.com/li/130014
+		http://togetter.com/li/133345
+		http://togetter.com/li/136645
+		http://togetter.com/li/140221
+		http://togetter.com/li/210287
+		http://togetter.com/li/241872
+		http://togetter.com/li/415632
 	)
 
 	def initialize
 		@data = Hash.new
-		parse
 	end
 
-	def parse
-		@@urls.each do |url|
-			doc = Nokogiri::HTML open(url)
-			title = doc.xpath('//a[@class="info_title"]/@title').first.value
-			tweets = []
-			doc.xpath('//div[@class="tweet"]').each do |tweet|
-				tweets << tweet.text
-			end
-			data = @data[url] = {}
-			data[:title] = title
-			data[:tweets] = tweets
-			return data
+	def parse(url)
+		doc = Nokogiri::HTML open(url)
+		title = doc.xpath('//a[@class="info_title"]/@title').first.value
+		tweets = []
+		doc.xpath('//div[@class="tweet"]').each do |tweet|
+			tweets << tweet.text
 		end
+		data = @data[url] = {}
+		data[:title] = title
+		data[:tweets] = tweets
 	end
 
-	def pages
-		pages = []
-		@@urls.each do |url|
-			data = @data[url]
-			title = data[:title]
-			tweets = data[:tweets]
-			lines = []
-			lines << '=' * title.bytesize
-			lines << title
-			lines << '=' * title.bytesize
+	def page(url)
+		parse url
+		data = @data[url]
+		title = data[:title]
+		tweets = data[:tweets]
+		lines = []
+		lines << '=' * title.bytesize
+		lines << title
+		lines << '=' * title.bytesize
+		lines << ''
+		tweets.each do |tweet|
+			lines << tweet
 			lines << ''
-			tweets.each do |tweet|
-				lines << tweet
-				lines << ''
-			end
-			
-			pages << (lines.join "\n")
+		end
+
+		return lines.join "\n"
+	end
+
+	def pages(urls)
+		pages = []
+		urls.each do |url|
+			pages << (page url)
 		end
 		return pages
 	end
@@ -56,7 +91,10 @@ end
 
 if __FILE__ == $0 then
 	nt = NinjaslayerTogetter.new
-	puts nt.pages
+	NinjaslayerTogetter::NeoSaitamaUrls.each_with_index do |url, i|
+		page = nt.page url
+		%x(cat << EOP > NeoSaitama_#{sprintf "%02d", i}.rst \n#{page}\n EOP)
+	end
 end
 
 
